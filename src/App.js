5b9efdf43df5b8adafed372ffc8c3d1535bc3556
JonathanRVega <Jonathaan.rv@gmail.com>
2020-11-11 11:03:12 -0700

    fixed functionality

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import { Component } from "react";
-import Buttons from "./Buttons"
+import Buttons from './Buttons'
 
 class App extends Component {
-  constructor(props){
-    super (props);
-  this.state = {
-    count: 0
-  }
-}
-
-increment() {
-    this.setState({
-      count: this.state.count + 1
-    });
-  }
-
-  decrement() {
-    this.setState({
-      count: this.state.count - 1
-    });
-  }
-
 
   render(){
   return (
     <div className="App">
-    <h1>{this.state.count}</h1>
-    <button onClick={() => this.increment()}>+</button>
-    <button onClick={() => this.decrement()}>-</button>
+    <Buttons/>
     </div>
   );
 }
--- src/Buttons.js
+++ src/Buttons.js
@@ -1,29 +1,64 @@
 import React, { Component } from 'react'
 
 class Buttons extends Component {
-    state = {
-        count: 0
+  constructor(props) {
+    super(props);
+    this.state = {
+        count: 0,
+        input: 0,
+      }
+      this.handleChange = this.handleChange.bind(this);
+      this.handleAdd = this.handleAdd.bind(this);
+      this.handleSubstract = this.handleSubstract.bind(this);
+    }
+    
+      handleChange(event) {
+        this.setState({
+          input: Number(event.target.value)
+        });
+      }
+      handleAdd(event) {
+        event.preventDefault()
+        this.setState({
+          count: this.state.input + this.state.count
+        });
       }
 
-    increment= (e) => {
+      handleSubstract(event) {
+        event.preventDefault()
+        this.setState({
+          count: this.state.count - this.state.input
+        });
+      }
+
+      increment(){
         this.setState({
           count: this.state.count + 1
         });
-        console.log(this.state.count)
       }
     
-      decrement = (e) => {
+      decrement(){
         this.setState({
           count: this.state.count - 1
         });
-        console.log(this.state.count)
       }
 
     render() {
         return (
-            <div>
-                <button onClick={this.state.increment}>+</button>
-                <button onClick={this.state.decrement}>-</button>
+            <div className="container">
+                <h1 className="center red-text text-darken-4">{this.state.count}</h1>
+                <div className="center">
+                  <button className="btn-floating btn-large waves-effect waves-light red margin" onClick={() => this.increment()}>+</button>
+                  <button className="btn-floating btn-large waves-effect waves-light red margin" onClick={() => this.decrement()}>-</button>
+                </div>
+                <div className="center">
+                  <form>
+                    <input className="form" type='number' step="1"
+                    onChange={this.handleChange} />
+                    <button className="waves-effect waves-light btn red margin" onClick={this.handleAdd} type='submit'>Add!</button>
+                    <button className="waves-effect waves-light btn red margin" onClick={this.handleSubstract} type='submit'>Substract!</button>
+                  </form>
+                </div>
             </div>
         )
     }
